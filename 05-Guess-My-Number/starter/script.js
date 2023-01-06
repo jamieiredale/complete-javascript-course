9899a271503c56cf54d792a02b2c4b42884d8007
jamieiredale <iredalejamie@gmail.com>
2023-01-06 20:43:32 +0000

    Implementing Game Logic
    Basic game logic not efficiently (lots of duplicate code). Still need to do the if statement for numbers that are 'too low'

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -11,6 +11,12 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+//We use let here instead of const as this variable will change.
+let score = 20;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
@@ -18,5 +24,21 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guess) {
     document.querySelector('.message').textContent = 'No number 🐣';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'You got it right 👍';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Its too high 🍾';
+      // score = score - 1; can write shorthand as score--
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game 😭';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Its too low 👩‍🏫';
+    score--;
+    document.querySelector('.score').textContent = score;
   }
 });
