f75d998654b31d13b2eac350f9cd092bdfa1c37e
jamieiredale <iredalejamie@gmail.com>
2023-01-08 15:37:49 +0000

    Working with classes
    Opening and closing a modal (event listener method)
    
    In practice using functionality of adding and removing classing to change the appearance of the UI.

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+//Limitations of query selector in that only query selector will be selected
+// const btnsOpenModal = document.querySelector('.show-modal');
+
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(btnsOpenModal);
+
+//DRY refactoring - creating a function to close the modal rather than repeating the code from Overlay and Close Modal Btn
+const closeModal = function () {
+  console.log('Button clicked');
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+//For is a loop expression
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
