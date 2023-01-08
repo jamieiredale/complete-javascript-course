c9eda06a143b1253f706a69cdce94f8affd17c2a
jamieiredale <iredalejamie@gmail.com>
2023-01-08 21:55:40 +0000

    Switching to the active player

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 //Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
@@ -16,7 +18,10 @@ score1El.textContent = 0;
 //Adding the hidden class to the Dice class
 diceEl.classList.add('hidden');
 
+//Storing large scores in an array
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -33,7 +38,21 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     //add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore;
+    // current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
+    //Using a ternary operator to reassign the active player. This allows us to switch from player 0 to player 1.
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    //Toggle both of the elements at the same time will ensure that it's only ever on one as the player--active is only associated with player0 in html as a class.
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
+});
+
+//Holding the Score - My Attempt (it's not adding the previous currentScore with the new ones)
+btnHold.addEventListener('click', function () {
+  document.getElementById(`score--${activePlayer}`).textContent = currentScore;
 });
