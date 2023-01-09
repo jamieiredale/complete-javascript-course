3ee901333798eac2fe8f1f21bfc8f5ee50948815
jamieiredale <iredalejamie@gmail.com>
2023-01-09 20:25:56 +0000

    Holding the current score

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -22,37 +22,67 @@ diceEl.classList.add('hidden');
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+//Creating a state for the game as a variable
+let playing = true;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  //Toggle both of the elements at the same time will ensure that it's only ever on one as the player--active is only associated with player0 in html as a class.
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', function () {
-  //1. Generating a Random Dice Roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
+  if (playing) {
+    //1. Generating a Random Dice Roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
 
-  //2. Display the dice
-  diceEl.classList.remove('hidden');
-  //Dyanmically loading one of the images based on the random number rolled
-  diceEl.src = `dice-${dice}.png`;
-  console.log(dice);
+    //2. Display the dice
+    diceEl.classList.remove('hidden');
+    //Dyanmically loading one of the images based on the random number rolled
+    diceEl.src = `dice-${dice}.png`;
+    console.log(dice);
 
-  //3. Check for rolled 1. If true, switch to next player
-  if (dice !== 1) {
-    //add dice to current score
-    currentScore += dice;
-    // current0El.textContent = currentScore;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
+    //3. Check for rolled 1. If true, switch to next player
+    if (dice !== 1) {
+      //add dice to current score
+      currentScore += dice;
+      // current0El.textContent = currentScore;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      //Switch to next Player - now using the function switchPlayer
+      switchPlayer();
+    }
     //Using a ternary operator to reassign the active player. This allows us to switch from player 0 to player 1.
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    //Toggle both of the elements at the same time will ensure that it's only ever on one as the player--active is only associated with player0 in html as a class.
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
   }
 });
 
 //Holding the Score - My Attempt (it's not adding the previous currentScore with the new ones)
 btnHold.addEventListener('click', function () {
-  document.getElementById(`score--${activePlayer}`).textContent = currentScore;
+  if (playing) {
+    console.log('Hold button');
+    //Add current score to active player's score
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    console.log(currentScore);
+    //check if player's score is >= 100
+
+    //Finish the game
+    if (scores[activePlayer] >= 10) {
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document.querySelector.classList.remove('player--active');
+    } else {
+      //Switch to the next player
+      switchPlayer();
+    }
+  }
 });
