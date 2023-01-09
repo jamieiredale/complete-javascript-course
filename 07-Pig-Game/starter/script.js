54bc38c76d7f6600e2499f7fd52816a3f64f9dfb
jamieiredale <iredalejamie@gmail.com>
2023-01-09 21:55:37 +0000

    Resetting the game
    Created an init function - variables defined within the function (scoping). See line 20 for declaring multiple variables (empty)

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -3,27 +3,43 @@
 //Selecting elements
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
+
 const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
-//selecting ID's instead of classes
-const score1El = document.getElementById('score--1');
+
 const diceEl = document.querySelector('.dice');
+
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-//Adding the hidden class to the Dice class
-diceEl.classList.add('hidden');
+//Research scoping - creating variables that live in a function
+let scores, currentScore, activePlayer, playing;
 
-//Storing large scores in an array
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-//Creating a state for the game as a variable
-let playing = true;
+//Starting conditions
+const init = function () {
+  //Storing large scores in an array
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  //Creating a state for the game as a variable
+  playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  //Adding the hidden class to the Dice class
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -44,7 +60,6 @@ btnRoll.addEventListener('click', function () {
     diceEl.classList.remove('hidden');
     //Dyanmically loading one of the images based on the random number rolled
     diceEl.src = `dice-${dice}.png`;
-    console.log(dice);
 
     //3. Check for rolled 1. If true, switch to next player
     if (dice !== 1) {
@@ -59,30 +74,30 @@ btnRoll.addEventListener('click', function () {
     }
     //Using a ternary operator to reassign the active player. This allows us to switch from player 0 to player 1.
   }
+  console.log(scores);
 });
 
 //Holding the Score - My Attempt (it's not adding the previous currentScore with the new ones)
 btnHold.addEventListener('click', function () {
   if (playing) {
-    console.log('Hold button');
     //Add current score to active player's score
     scores[activePlayer] += currentScore;
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
-    console.log(currentScore);
-    //check if player's score is >= 100
-
-    //Finish the game
-    if (scores[activePlayer] >= 10) {
+    //Check if player's score is >= 100 & Finish the game
+    if (scores[activePlayer] >= 100) {
       playing = false;
       diceEl.classList.add('hidden');
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.add('player--winner');
-      document.querySelector.classList.remove('player--active');
+      document.querySelector.classList.remove('.player--active');
     } else {
       //Switch to the next player
       switchPlayer();
     }
   }
 });
+//JS will call the init function so no need to add ()
+
+btnNew.addEventListener('click', init);
